6acb66eeaab126c79a5f9e84ac787ebbe89f1aec
Bubblezq <1625242167@qq.com>
2020-05-13 22:34:46 +0800

    update click

--- MyApplication/app/src/main/java/com/example/myapplication/MapActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MapActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.content.Intent;
 
 public class MapActivity extends Activity {
@@ -13,12 +12,112 @@ public class MapActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
 
-        ImageButton btn=findViewById(R.id.yihongyuan);
-        btn.bringToFront();
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button btn1=findViewById(R.id.yihongyuan);
+        btn1.bringToFront();
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(MapActivity.this,YiHongYuanActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btn2=findViewById(R.id.xiaoxiangguan );
+        btn2.bringToFront();
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MapActivity.this,XiaoXiangGuanActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btn3=findViewById(R.id.xingqinbieshu );
+        btn3.bringToFront();
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MapActivity.this,XingQinBieShuActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btn4=findViewById(R.id.longcuian);
+        btn4.bringToFront();
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MapActivity.this,LongCuiAnActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btn5=findViewById(R.id.zhuijinlou);
+        btn5.bringToFront();
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MapActivity.this,ZhuiJinLouActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btn6=findViewById(R.id.hengwuyuan);
+        btn6.bringToFront();
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MapActivity.this,HengWuYuanActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btn7=findViewById(R.id.hongxiangpu);
+        btn7.bringToFront();
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MapActivity.this,HongXiangPuActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btn8=findViewById(R.id.daoxiangcun);
+        btn8.bringToFront();
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MapActivity.this,DaoXiangCunActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btn9=findViewById(R.id.liaofengxuan);
+        btn9.bringToFront();
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MapActivity.this,LiaoFengXuanActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btn10=findViewById(R.id.qiushuangzhai);
+        btn10.bringToFront();
+        btn10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MapActivity.this,QiuShuangZhaiActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btn11=findViewById(R.id.zhanhuaming);
+        btn11.bringToFront();
+        btn11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MapActivity.this,QiuShuangZhaiActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btn12=findViewById(R.id.yuanxiaodengmi);
+        btn12.bringToFront();
+        btn12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MapActivity.this,QiuShuangZhaiActivity.class);
                 startActivity(intent);
             }
         });
---
+++ MyApplication/app/src/main/java/com/example/myapplication/XiaoXiangGuanActivity.java
@@ -0,0 +1,93 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class XiaoXiangGuanActivity extends Activity {
+    private int text_count=0;
+    private float originalX;
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.xiaoxiangguan);
+
+        /*Button b2=findViewById(R.id.unknown);
+        Button b3=findViewById(R.id.more);*/
+
+        final TextView tv=findViewById(R.id.dialog);
+        tv.bringToFront();
+        final ImageView iv=findViewById(R.id.displayImage);
+        iv.bringToFront();
+
+        ImageButton btn=findViewById(R.id.imageButton);
+        btn.bringToFront();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TypedArray ta=getResources().obtainTypedArray(R.array.text);
+                int id=ta.getResourceId(text_count,-1);
+                tv.setText(id);
+                tv.setVisibility(View.VISIBLE);
+                iv.setVisibility(View.VISIBLE);
+                text_count=text_count+1;
+                ta.recycle();
+            }
+        });
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TypedArray ta=getResources().obtainTypedArray(R.array.text);
+                int id=ta.getResourceId(text_count,-1);
+                if(id==R.string.occupation) {
+                    text_count=0;
+                    tv.setVisibility(View.GONE);
+                    iv.setVisibility(View.GONE);
+                    ta.recycle();
+                    return;
+                }
+                tv.setText(id);
+                tv.setVisibility(View.VISIBLE);
+                text_count=text_count+1;
+                ta.recycle();
+            }
+        });
+
+        Button b1=findViewById(R.id.back);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        iv.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction()==MotionEvent.ACTION_DOWN)
+                {
+                    originalX=event.getRawX();
+                    Log.v("originalX:",String.valueOf(originalX));
+                }
+                if(event.getAction()==MotionEvent.ACTION_MOVE)
+                {
+                    float currentX=event.getRawX();
+                    Log.v("currentX:",String.valueOf(currentX));
+                    iv.setX(iv.getX()+currentX-originalX);
+                    originalX=currentX;
+                }
+                return true;
+            }
+        });
+    }
+}
